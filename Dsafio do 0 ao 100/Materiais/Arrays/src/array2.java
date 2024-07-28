8b07f5d1f335f7189a52cc989734f833b87c91eb
Matheus Vinicius <matheusviniciuscaieiras@gmail.com>
2024-07-28 19:06:12 -0300

    "Array implent"

---
+++ Dsafio do 0 ao 100/Materiais/Arrays/src/array2.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author Matheus
+ * @version 001 
+ */
+public class array2{
+
+  public static void main(String[] args) throws Exception {
+
+    String [ ] paises = new String[5];
+     paises[0]="Brasil";
+     paises[1]="Argentina";
+     paises[2]="Belise";
+     paises[3]="Argelia";
+     paises[4]="Africa sul";
+   
+
+  for(String lista :paises){
+
+    System.out.println(lista);
+
+       }
+    }
+
+
+  }
